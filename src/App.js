44ec1f03d6f49b3098cf4a05bcce3a784401707c
akhror-valiev <ahror.valiev@gmail.com>
2021-11-28 19:30:36 +0500

    first action

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Feature from "./components/Feature";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Offer from "./components/Offer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Navbar />
+			<Header />
+			<Feature />
+			<Offer />
+			<About />
+			<Contact />
+		</div>
+	);
 }
 
 export default App;
